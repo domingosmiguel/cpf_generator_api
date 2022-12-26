a0d96578088be97a16d7036374ae2fe3032ac034
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2022-12-25 23:54:48 -0300

    chore: rename constant for semantics sake

--- src/assets/functions/cpfGenerator.js
+++ src/assets/functions/cpfGenerator.js
@@ -3,19 +3,19 @@ import firstDigit from './firstDigitCPF.js';
 import getRandomArbitraryNumber from './getRandomArbitraryNumber.js';
 import secondDigit from './secondDigitCPF.js';
 
-export default function cpfGenerator(punctuation) {
+export default function cpfGenerator(mask) {
   const numbers = [];
 
   for (let i = 0; i < 9; i++) {
     numbers.push(getRandomArbitraryNumber(0, 9).toFixed());
   }
   if (allDigitsAreEqual(numbers)) {
-    return cpfGenerator(punctuation);
+    return cpfGenerator(mask);
   }
   numbers.push(firstDigit(numbers));
   numbers.push(secondDigit(numbers));
 
-  if (punctuation) {
+  if (mask) {
     numbers.splice(9, 0, '-');
     numbers.splice(6, 0, '.');
     numbers.splice(3, 0, '.');
