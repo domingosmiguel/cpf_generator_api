c5f96befffe8361a21a78ee74836de129ec8a556
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2022-12-26 01:13:24 -0300

    refact: add case insensitivity to routes query

--- src/controllers/cnpjControllers.js
+++ src/controllers/cnpjControllers.js
@@ -1,7 +1,7 @@
 import cnpjGenerator from '../assets/functions/cnpjGenerator.js';
 
 export const getNewCnpj = (req, res) => {
-  const mask = req.query.mask === 'true' ? true : false;
+  const mask = req.query.mask?.toLowerCase() === 'true' ? true : false;
   const newCNPJ = cnpjGenerator(mask);
   res.send(newCNPJ);
 };
--- src/controllers/cpfControllers.js
+++ src/controllers/cpfControllers.js
@@ -1,7 +1,7 @@
 import cpfGenerator from '../assets/functions/cpfGenerator.js';
 
 export const getNewCpf = (req, res) => {
-  const mask = req.query.mask === 'true' ? true : false;
+  const mask = req.query.mask?.toLowerCase() === 'true' ? true : false;
   const newCPF = cpfGenerator(mask);
   res.send(newCPF);
 };
